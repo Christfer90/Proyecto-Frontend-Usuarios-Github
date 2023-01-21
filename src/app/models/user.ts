9e7367a57ac3a636e4aa74a5dd3af157aa700e56
Christfer90 <cfpc9005@gmail.com>
2023-01-21 08:51:04 -0500

    Se crea modelo de usuarios

---
+++ src/app/models/user.ts
@@ -0,0 +1,13 @@
+export class User {
+    id: number;
+    login: string;
+    avatar_url: string;
+    created_at: string;
+    name: string;
+    location: string;
+    twitter_username: string;
+    blog: string;
+    public_repos: string;
+    followers: number;
+    following: number
+}
