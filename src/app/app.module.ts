ebfc546f398d3b4f82545ac5d0a7044d96b6a3f0
Christfer90 <cfpc9005@gmail.com>
2023-01-21 07:58:24 -0500

    Versión inicial de proyecto frontend, se crean los componentes user y profiles, se instala bootstrap

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,14 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { UserComponent } from './components/user/user.component';
+import { ProfilesComponent } from './components/profiles/profiles.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    UserComponent,
+    ProfilesComponent
   ],
   imports: [
     BrowserModule
